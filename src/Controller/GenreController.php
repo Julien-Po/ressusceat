a9928445698c5ff5670de1a75417a275a2859c7d
Julien <poirierjulien30@gmail.com>
2024-10-26 16:14:05 +0200

    Ajout du CRUD et du form genre

--- src/Controller/GenreController.php
+++ src/Controller/GenreController.php
@@ -2,17 +2,87 @@
 
 namespace App\Controller;
 
+use App\Entity\Genre;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Form\GenreType;
+use App\Repository\GenreRepository;
 
 class GenreController extends AbstractController
 {
-    #[Route('admin/creategenre', name: 'genre_create')]
-    public function create(): Response
+    #[Route('admin/newgenre', name: 'app_genre', methods:['GET','POST'])]
+    public function createGenre(Request $request, EntityManagerInterface $manager): Response
     {
-        return $this->render('genre/index.html.twig', [
-            'controller_name' => 'GenreController',
+
+            $genre = new Genre();
+            $form = $this->createForm(GenreType::class, $genre);
+
+            $form->handleRequest($request);
+            if ($form->isSubmitted() && $form->isValid())
+            {
+               $genre = $form->getData();
+
+               $manager->persist($genre);
+               $manager->flush();
+
+               return $this->redirectToRoute('view_genre');
+            }
+
+            return $this->render('genre/new.html.twig',[
+                'form' => $form->createView()
+            ]);
+    }
+
+    #[Route('admin/genre', name :'view_genre', methods:['GET'])]
+    public function displayGenre(GenreRepository $repository) : Response
+
+    {
+        $genre = $repository->findAll();
+
+        return $this->render('genre/displaygenre.html.twig', [
+            'genres'=>$genre
         ]);
+    }
+
+    #[Route('admin/genre/update/{id}', name: 'edit_genre', methods: ['GET', 'POST'])]
+    public function editGenre(GenreRepository $repository, int $id, Request $request, EntityManagerInterface $manager): Response
+    {
+        $genre = $repository->findOneBy(["id" => $id]);
+        
+        if (!$genre) {
+            throw $this->createNotFoundException('Genre not found');
+        }
+    
+        $form = $this->createForm(GenreType::class, $genre);
+        $form->handleRequest($request);
+    
+        if ($form->isSubmitted() && $form->isValid()) {
+            $manager->persist($genre);
+            $manager->flush();
+    
+            return $this->redirectToRoute('view_genre');
+        }
+    
+        return $this->render('genre/edit.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+    
+
+    #[Route('admin/genre/delete/{id}', name : 'delete_genre', methods: ['GET'])]
+    public function delete(EntityManagerInterface $manager, Genre $genre) : Response
+    {
+        $manager->remove($genre);
+        $manager->flush();
+
+            $this->addFlash(
+                'success',
+                'Votre genre a été supprimé avec succès !'
+            );
+
+        return $this->redirectToRoute('view_genre');
     }
 }
---
+++ src/Form/GenreType.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Genre;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+class GenreType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', TextType::class, [
+                'label' => 'Nom',
+                'attr' => [
+                    'autocomplete' => 'off',
+                ],
+            ])
+            ->add('submit', SubmitType::class, 
+            ["label" => "Envoyer",
+            "attr" => [
+                'class' => 'button'
+            ]])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Genre::class,
+        ]);
+    }
+}
