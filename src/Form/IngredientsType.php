54b5f0e043fc81c5897f60c3584e1074a01b017c
Julien <poirierjulien30@gmail.com>
2024-11-06 14:40:13 +0100

    Ajout de style sur la page create ingredient

--- src/Form/IngredientsType.php
+++ src/Form/IngredientsType.php
@@ -4,8 +4,10 @@ namespace App\Form;
 
 use App\Entity\Genre;
 use App\Entity\Ingredients;
-use App\Entity\Recipes;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -15,17 +17,32 @@ class IngredientsType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('image')
+            ->add('name', TextType::class, [
+                'attr' => [
+                    'autocomplete' => 'off',
+                ],
+                "label" => "Nom",
+                'label_attr' => [
+                    'class' => 'textStyle'
+                ],
+            ])
+            ->add('image', FileType::class, [
+                "mapped" => false, 
+                "required" => false
+            ])
             ->add('genre', EntityType::class, [
                 'class' => Genre::class,
-                'choice_label' => 'id',
+                'choice_label' => 'name',
+                'label_attr' => [
+                    'class' => 'textStyle'
+                ],
             ])
-            ->add('recipes', EntityType::class, [
-                'class' => Recipes::class,
-                'choice_label' => 'id',
-                'multiple' => true,
-            ])
+            ->add('submit', SubmitType::class, 
+            ["label" => "Envoyer",
+            "attr" => [
+                'class' => 'button'
+            ]])
+
         ;
     }
 
